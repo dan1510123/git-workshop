21876a44b0030544e928329fc7fb50476a0f5a02
Daniel <dan1510123@gmail.com>
2018-01-21 14:40:17 -0500

    multiples of 7 say bizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,19 @@
 
 def fizzbuzz(n):
     for i in range(1,n+1):
-        if i % 15 == 0:
-            print "fizzbuzz"
+        if i % 105 == 0:
+            print("fizzbuzzbizz")
+        elif i % 35 == 0:
+            print("buzzbizz")
+        elif i % 21 == 0:
+            print("fizzbizz")
+        elif i % 15 == 0:
+            print("fizzbuzz")
         elif i % 3 == 0:
-            print "fizz"
+            print("fizz")
         elif i % 5 == 0:
-            print "buzz"
+            print("buzz")
+        elif i % 7 == 0:
+            print("bizz")
         else:
-            print i
+            print(i)
